24b81f14ec452b57aa81a48dc3ac928e99d6ad3e
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-09 08:12:35 +0100

    fix tests

--- test.js
+++ test.js
@@ -3,5 +3,6 @@ var assert = require('assert');
 var tildify = require('./index');
 
 it('should convert path to tilde style', function () {
+	process.env.HOME = '/Users/sindresorhus';
 	assert.equal(tildify('/Users/sindresorhus/dev/tildify'), '~/dev/tildify');
 });

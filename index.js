58a3a2c0bebfe34dd9f9ca67f702c541a544d768
Fritz Lin <uxfritz@163.com>
2015-09-18 14:06:31 +0700

    Close #4 PR: strict replace, fix #3.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
+var path = require('path');
 var osHomedir = require('os-homedir');
 var home = osHomedir();
 
 module.exports = function (str) {
-	return str.replace(home, '~');
+	str += path.sep;
+	str = str.replace(home + path.sep, '~' + path.sep);
+	str = str.slice(0, -1);
+	return str;
 };
--- test.js
+++ test.js
@@ -6,9 +6,21 @@ var osHomedir = require('os-homedir');
 var home = osHomedir();
 
 test(function (t) {
+	var fixture = home;
+	t.assert(tildify(fixture) === '~');
+	t.end();
+});
+
+test(function (t) {
 	var fixture = path.resolve(home, 'tildify');
 	t.assert(tildify(fixture)[0] === '~');
 	t.assert(/tildify$/.test(tildify(fixture)));
 	t.assert(tildify(fixture) !== fixture);
+	t.end();
+});
+
+test(function (t) {
+	var fixture = path.resolve(home + 'foo', 'tildify');
+	t.assert(tildify(fixture) === fixture);
 	t.end();
 });

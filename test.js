bce89adf1e1318e7c62086482d16706cd05f7b4e
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-12 14:45:52 +0700

    meta tweaks

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
 'use strict';
-const test = require('ava');
-const tildify = require('./');
 const path = require('path');
+const test = require('ava');
 const osHomedir = require('os-homedir');
+const m = require('./');
 const home = osHomedir();
 
 test('tildify home', t => {
 	const fixture = home;
-	t.is(tildify(fixture), '~');
+	t.is(m(fixture), '~');
 });
 
 test('tildify path', t => {
 	const fixture = path.resolve(home, 'tildify');
-	t.is(tildify(fixture)[0], '~');
-	t.true(/tildify$/.test(tildify(fixture)));
-	t.not(tildify(fixture), fixture);
+	t.is(m(fixture)[0], '~');
+	t.true(/tildify$/.test(m(fixture)));
+	t.not(m(fixture), fixture);
 });
 
 test('ensure only a fully matching path is replaced', t => {
 	const fixture = path.resolve(`${home}foo`, 'tildify');
-	t.is(tildify(fixture), fixture);
+	t.is(m(fixture), fixture);
 });
 
 test('ignore relative paths', t => {
 	const fixture = 'tildify';
-	t.is(tildify(fixture), fixture);
+	t.is(m(fixture), fixture);
 });
 
 test('only tildify when home is at the start of a path', t => {
 	const fixture = path.join('tildify', home);
-	t.is(tildify(fixture), fixture);
+	t.is(m(fixture), fixture);
 });
